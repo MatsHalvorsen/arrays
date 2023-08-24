6b1feb98f090ed87229eac0f588dc32f7b3c60d3
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-08-24 13:22:17 +0200

    added some challanges

--- app.js
+++ app.js
@@ -19,3 +19,22 @@ function getValue() {
    }
 }
 console.log(arr.join(''))
+
+//find the most frequent item in an array
+var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3,];
+    var mostFrequent = 1;
+    var m = 0;
+    var item;
+    for (var i = 0; i < arr1.length; i++) {
+            for (var j = i; j < arr1.length; j++) {
+                if (arr1[i] == arr1[j])
+                m++;
+            if (mostFrequent < m) {
+                mostFrequent = m;
+                item = arr1[i];
+            }
+        }
+        m = 0;
+    }
+        console.log(item+" ( " +mostFrequent +" times ) ") ;            
+
